8230c1f98b4b705dc7143a5d9f18174b0e44cace
Ohno Shoya <shoya.inf@gmail.com>
2020-07-25 05:06:12 +0000

    0725

--- resources/views/front/list.blade.php
+++ resources/views/front/list.blade.php
@@ -24,7 +24,7 @@
                               コメント：{{$item->comment}} <br>
                               {{$item->detail}} <br>
                               <div class="text-left" >
-                            　   <a href="{{ action('CommentController@create', ['id' => $item->id]) }}">コメントをする。</a>
+                            　   <a href="{{ action('CommentController@create', ['id' => $item->id]) }}">コメントをする</a>
                               </div>
                               <div class="text-left" >
                             　   <a href="{{ action('CommentController@show', ['id' => $item->id]) }}">コメントを見る</a>
